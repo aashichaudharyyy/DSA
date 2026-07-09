07dfa7b74629fb9066384e6639decc5fbb4aecbc
Aashi Chaudhary <aashi.chaudharryy@gmail.com>
2026-07-09 18:26:03 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.cpp
+++ LeetCode/Two-Sum/Solution.cpp
@@ -1,14 +1,18 @@
-1//brute-force approach
+1//hash-map approach 
 2
 3class Solution {
 4public:
 5    vector<int> twoSum(vector<int>& nums, int target) {
-6        for (int i=0;i<nums.size();i++){
-7            for (int j=i+1;j<nums.size();j++){
-8                if (nums[i]+nums[j] == target)
-9                    return {i,j};
-10            };
-11        };
-12        return {};
-13    };
-14};
+6        unordered_map<int, int> hashMap;
+7        for (int i=0;i<nums.size();i++){
+8            hashMap[nums[i]] = i;
+9        }
+10
+11        for (int i=0;i<nums.size();i++){
+12            int comp = target - nums[i];
+13            if (hashMap.find(comp) != hashMap.end() && hashMap[comp] != i)
+14                return {i,hashMap[comp]};
+15        };
+16        return {};
+17    };
+18};
