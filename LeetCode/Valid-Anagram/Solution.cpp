1d2653b67b0abea7636f382804198354d17f7f1a
Aashi Chaudhary <aashi.chaudharryy@gmail.com>
2026-07-11 12:32:52 +0530

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.cpp
+++ LeetCode/Valid-Anagram/Solution.cpp
@@ -1,9 +1,19 @@
 1class Solution {
 2public:
 3    bool isAnagram(string s, string t) {
-4        sort(s.begin(),s.end());
-5        sort(t.begin(),t.end());
-6        if(s==t) return true;
-7        else return false;
-8    }
-9};
+4        if (s.size() != t.size()) return false;
+5
+6        vector<int> freq(26,0);
+7
+8        for(int i=0;i<s.size();i++){
+9            freq[s[i] - 'a']++;
+10            freq[t[i] - 'a']--;
+11        }
+12
+13        for(int x : freq){
+14            if(x!=0) return false;
+15        }
+16
+17        return true;
+18    }
+19};
