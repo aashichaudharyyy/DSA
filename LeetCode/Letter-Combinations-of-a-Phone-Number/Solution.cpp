478a1f4926320e92940cab5036db7e0d8135bfba
Aashi Chaudhary <aashi.chaudharryy@gmail.com>
2026-08-20 16:32:07 +0530

    Add solution for Letter Combinations of a Phone Number

--- LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.cpp
+++ LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.cpp
@@ -1,34 +1,33 @@
-1class Solution {
-2public:
-3    vector<string> ans;
-4    vector<string> letters = {
-5        "", "", "abc", "def", "ghi",
-6        "jkl", "mno", "pqrs", "tuv", "wxyz"
-7    };
-8
-9    void solve(string digits, int index, string current){
-10         if(index == digits.size()) {
-11            ans.push_back(current);
-12            return;
-13        }
-14        
-15        string str = letters[digits[index] - '0'];
-16        for(char ch : str) {
-17            // include
-18            current.push_back(ch);
-19            solve(digits, index + 1, current);
-20
-21            // exclude
-22            current.pop_back();
-23        }
-24    }
-25    vector<string> letterCombinations(string digits) {
-26        if(digits.empty())
-27            return {};
-28
-29        string current;
-30        solve(digits, 0, current);
-31
-32        return ans;
-33    }
-34};
+class Solution {
+private:
+    void solve(string digit, string output, int i, vector<string>& ans,
+               string mapping[]) {
+        if (i >= digit.length()) {
+            ans.push_back(output);
+            return;
+        }
+
+        int num = digit[i] - '0';
+        string value = mapping[num];
+
+        for (int j = 0; j < value.length(); j++) {
+            output.push_back(value[j]);
+            solve(digit, output, i + 1, ans, mapping);
+            output.pop_back();
+        }
+    }
+
+public:
+    vector<string> letterCombinations(string digit) {
+        vector<string> ans;
+        if (digit.length() == 0) {
+            return ans;
+        }
+        int i = 0;
+        string output;
+        string mapping[10] = {"",    "",    "abc",  "def", "ghi",
+                              "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        solve(digit, output, i, ans, mapping);
+        return ans;
+    }
+};
