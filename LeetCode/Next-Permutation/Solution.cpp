91583e985ceacfa7640395f4a3161421aeb4f18c
Aashi Chaudhary <aashi.chaudharryy@gmail.com>
2026-08-21 16:03:29 +0530

    Add solution for Next Permutation

--- LeetCode/Next-Permutation/Solution.cpp
+++ LeetCode/Next-Permutation/Solution.cpp
@@ -1,32 +1,33 @@
-1class Solution {
-2public:
-3
-4    void swap(int i, int j, vector<int>& nums){
-5        int temp = nums[i];
-6        nums[i] = nums[j];
-7        nums[j] = temp;
-8    }
-9
-10    void nextPermutation(vector<int>& nums) {
-11        int pivot,max;
-12        bool found = false;
-13        for(int i=nums.size()-2;i>=0;i--){
-14            if(nums[i] < nums[i+1]){
-15                found = true;
-16                pivot = i;
-17                int j = nums.size() - 1;
-18                while(nums[j] <= nums[pivot]) {
-19                    j--;
-20                }
-21                swap(pivot,j,nums);
-22                //asc right portion
-23                sort(nums.begin()+i+1,nums.end());
-24                break;
-25            }
-26        }
-27
-28        if (not found){
-29            sort(nums.begin(),nums.end());
-30        }
-31    }
-32};
+1//find increasing -> find next greater in right & swap -> sort right -> if not found -> sort full
+2
+3class Solution {
+4public:
+5    void swap(int i, int j, vector<int>& nums){
+6        int temp = nums[i];
+7        nums[i] = nums[j];
+8        nums[j] = temp;
+9    }
+10
+11    void nextPermutation(vector<int>& nums) {
+12        int pivot,max;
+13        bool found = false;
+14        for(int i=nums.size()-2;i>=0;i--){
+15            if(nums[i] < nums[i+1]){
+16                found = true;
+17                pivot = i;
+18                int j = nums.size() - 1;
+19                while(nums[j] <= nums[pivot]) {
+20                    j--;
+21                }
+22                swap(pivot,j,nums);
+23                //asc right portion
+24                sort(nums.begin()+i+1,nums.end());
+25                break;
+26            }
+27        }
+28
+29        if (not found){
+30            sort(nums.begin(),nums.end());
+31        }
+32    }
+33};
